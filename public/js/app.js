2507f0942fa67fe4db5abadb0ad405c6de969abd
Eric Wishom <eric.wishom@eric.lan>
2020-06-30 00:08:03 -0400

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-console.log('Public Client Side Javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const searchTerm = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
